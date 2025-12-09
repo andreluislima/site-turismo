4239b96324bc9b80a16549093da9bb9aa1d2b8a0
andreluislima <andrelimag89@gmail.com>
2025-12-09 20:12:38 -0300

    cria footer

--- agencia-viagens/src/layout/default-layout/default-layout.ts
+++ agencia-viagens/src/layout/default-layout/default-layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
+import { Hero } from "../../components/hero/hero";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-default-layout',
-  imports: [Navbar],
+  imports: [Navbar, Hero, Footer],
   templateUrl: './default-layout.html',
   styleUrl: './default-layout.scss',
 })
