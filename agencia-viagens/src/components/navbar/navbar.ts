bcfde0cdc8f8d00be32e3cbf85c3d4dfe46422e0
andreluislima <andrelimag89@gmail.com>
2025-12-12 11:41:27 -0300

    fix: ajusta navbar

--- agencia-viagens/src/components/navbar/navbar.ts
+++ agencia-viagens/src/components/navbar/navbar.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
 
+  navPages = [
+    {
+      "item": "pacotes",
+      "img": "assets/img/icons/navbar/icons-mala.png"
+    },
+    {
+      "item": "soluções de viagem",
+      "img": "assets/img/icons/navbar/icons-lampada.png"
+    },
+    {
+      "item": "passagens",
+      "img": "assets/img/icons/navbar/icons-ingressos.png"
+    },
+    {
+      "item": "passeios e experiencias",
+      "img": "assets/img/icons/navbar/icons-mundo.png"
+    },
+    {
+      "item": "sobre",
+      "img": "assets/img/icons/navbar/icons-pessoa.png"
+    },
+     
+  ];
 }
