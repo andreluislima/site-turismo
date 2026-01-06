a87d5388ea5cffb905b8c1c2d76027bf5f23388a
andreluislima <andrelimag89@gmail.com>
2026-01-05 21:41:05 -0300

    fix: ajusta header

--- agencia-viagens/src/layout/default-layout/default-layout.ts
+++ agencia-viagens/src/layout/default-layout/default-layout.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Navbar } from "../../components/navbar/navbar";
 import { Hero } from "../../components/hero/hero";
-import { Footer } from "../../components/footer/footer";
 import { Destaques } from '../../views/pages/destaques/destaques';
 import { Catalogo } from "../../views/pages/catalogo/catalogo";
 import { Depoimentos } from "../../views/pages/depoimentos/depoimentos";
@@ -10,7 +8,7 @@ import { DicasViagem } from "../../views/pages/dicas-viagem/dicas-viagem";
 
 @Component({
   selector: 'app-default-layout',
-  imports: [Navbar, Hero, Footer, Destaques, Catalogo, Depoimentos, DicasViagem],
+  imports: [Hero, Destaques, Catalogo, Depoimentos, DicasViagem],
   templateUrl: './default-layout.html',
   styleUrl: './default-layout.scss',
 })
--- agencia-viagens/src/views/pages/passagens/passagens.ts
+++ agencia-viagens/src/views/pages/passagens/passagens.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Depoimentos } from "../depoimentos/depoimentos";
 import { BannerTitlePages } from "../../../components/banner-title-pages/banner-title-pages";
-import { Card } from "../../../components/card/card";
 import { CommonModule } from '@angular/common';
 import { passagensNacionais } from '../../../app/data/data.passagens_nacionais';
 import { passagensInternacionais } from '../../../app/data/data.passagens_internacionais';
@@ -9,7 +8,7 @@ import { CardPassagens } from "../../../components/card-passagens/card-passagens
 
 @Component({
   selector: 'app-passagens',
-  imports: [Depoimentos, BannerTitlePages, Card, CommonModule, CardPassagens],
+  imports: [Depoimentos, BannerTitlePages, CommonModule, CardPassagens],
   templateUrl: './passagens.html',
   styleUrl: './passagens.scss',
 })
