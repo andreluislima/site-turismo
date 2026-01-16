747b3f437157de847ae12d1106205b4935eff7cc
andreluislima <andrelimag89@gmail.com>
2026-01-15 22:18:30 -0300

    feat: cria formulario

--- agencia-viagens/src/components/form/form.ts
+++ agencia-viagens/src/components/form/form.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './form.html',
   styleUrl: './form.scss',
 })
