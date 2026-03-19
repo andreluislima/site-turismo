3197d685dd0b516462ee0899a8e14c88b655d2ec
andreluislima <andrelimag89@gmail.com>
2026-03-19 14:47:07 -0300

    feat(main): implementa e configura o queryClient para controle de cache e requisicoes da API.

--- agente-viagem/src/App.tsx
+++ agente-viagem/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import MainRoutes from './routes'
 
 function App() {
  
   return (
     <>
-       <h1>Teste</h1>
+       <MainRoutes/>
     </>
   )
 }
--- agente-viagem/src/main.tsx
+++ agente-viagem/src/main.tsx
@@ -1,15 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 30
+    }
+  }
+})
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
