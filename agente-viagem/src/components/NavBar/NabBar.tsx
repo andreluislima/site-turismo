d93e55d765aaafa5bed3bafe08826a1417ff44a3
andreluislima <andrelimag89@gmail.com>
2026-03-20 10:33:23 -0300

    feat(components): adiciona pages ao navbar.

--- agente-viagem/src/components/NavBar/NabBar.tsx
+++ agente-viagem/src/components/NavBar/NabBar.tsx
@@ -1,8 +1,47 @@
+import { useState } from "react";
 import LogoImage from "../../assets/img/logo2.svg"
 import "./NabBar.scss";
 export default function NavBar() {
 
-    
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu(){
+    setIsMenuOpen((prev) => !prev);
+  }
+
+  function closeMenu(){
+    setIsMenuOpen(false);
+  }
+
+  const navPages = [
+     {
+      item: "pacotes",
+      img: "/assets/img/icons/navbar/icons-mala.png",
+      path: "/pacotes",
+    },
+    {
+      item: "soluções de viagem",
+      img: "/assets/img/icons/navbar/icons-lampada.png",
+      path: "/solucoes",
+    },
+    {
+      item: "passagens",
+      img: "/assets/img/icons/navbar/icons-ingressos.png",
+      path: "/passagens",
+    },
+    {
+      item: "passeios",
+      img: "/assets/img/icons/navbar/icons-mundo.png",
+      path: "/passeios",
+    },
+    {
+      item: "sobre",
+      img: "/assets/img/icons/navbar/icons-pessoa.png",
+      path: "/sobre",
+    },
+  ];
+  
+
   return (
     <>
       <nav className="navbar navbar-expand-md fixed-top">
